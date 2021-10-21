3fb6a14cccbcf0dd2b9ad08b50b885774d25d798
BrayanSantosR <brayansan93@hotmail.es>
2021-10-20 23:37:43 -0500

    agregar informacion de repaso js

---
+++ script/script.js
@@ -0,0 +1,32 @@
+let nombrePlanetas = Array ('Tierra','Marte','Jupiter')
+let distancia =[5,3,'67']
+const tamaño =[24, 50, 60]
+
+console.log(nombrePlanetas)
+console.log(distancia)
+console.log(tamaño)
+
+let i=0;
+while(i<= nombrePlanetas.length - 1){
+    console.log(i)
+    console.log(nombrePlanetas[i]);
+    i=i+1
+}
+
+for (let index = 0; index < nombrePlanetas.length; index++) {
+    console.log(nombrePlanetas[index]);
+    }
+
+ for(const key in nombrePlanetas){
+        console.log(nombrePlanetas[key])}
+        
+for(const planeta of nombrePlanetas){
+    console.log(planeta)
+}
+for(const key in distancia){
+    if(distancia[key]==='5'){
+    console.log('Se encontró la distancia')}
+    else{
+    console.log('No se encontró la distancia')
+    }
+}
